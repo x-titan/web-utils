03e0044de4f4ed2eb71bc5510414958408e868eb
NoNameTitan <50516342+x-titan@users.noreply.github.com>
2021-06-16 12:10:33 +0500

    init

---
+++ css.js
@@ -0,0 +1,44 @@
+//#region Types
+/** @typedef {HTMLElement | HTMLDivElement | Element} targetElements */
+//#endregion
+
+const classList = (x, y, z) => { if (Array.isArray(z)) x.classList[y](...z); else x.classList[y](z) },
+  checker = (x, y) => target instanceof HTMLElement && "object" === typeof style && style !== null;
+/**
+ * @param {targetElements} target
+ * @param {string | string[]} css
+ */
+export function add(target, css) { classList(target, "add", css) }
+/**
+ * @param {targetElements} target
+ * @param {string | string[]} css
+ */
+export function remove(target, css) { classList(target, "remove", css) }
+/**
+ * @param {targetElements} target
+ * @param {string} css
+ */
+export function contains(target, css) { return target.classList.contains(css) }
+/**
+ * @param {targetElements} target
+ * @param {string} css
+ * @param {boolean} [force]
+ */
+export function toggle(target, css, force) { return target.classList.toggle(css, force) }
+/**
+ * @param {HTMLElement | HTMLDivElement} target
+ * @param {CSSStyleDeclaration} css
+ */
+export function styler(target, style) {
+  if (checker(target, style)) for (const k in style)
+    if (Object.hasOwnProperty.call(style, k)) target.style[k] = style[k]
+  return target
+}
+/**
+ * @param {HTMLElement | HTMLDivElement} target
+ * @param {CSSStyleDeclaration} css
+ */
+styler.set = (target, style) => {
+  if (checker(target, style)) for (const k in style)
+    if (Object.hasOwnProperty.call(style, k)) target.style.setProperty(k, style[k])
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+import { search, device, scrollTo } from "./document.js"
+import { add, remove, contains, toggle, styler } from "./css.js"
+
+export { search, device, scrollTo, add, remove, contains, toggle, styler }
