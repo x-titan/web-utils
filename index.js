f3e121f0b5fb290c5e9649fa0af547f6ef8d27f8
Aset Telmanov <50516342+x-titan@users.noreply.github.com>
2021-07-01 13:25:53 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ import { search, device, scrollTo } from "./document.js"
 import { add, remove, contains, toggle, styler } from "./css.js"
 import "./polifill.js"
 
+console.log("%cThis site use web-utils\nPowered by https://x-titan.github.io", "font-size:2em")
+
 export { search, device, scrollTo, add, remove, contains, toggle, styler }
