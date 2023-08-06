6aba2ca93415e024dc5d26e32b0b41a8c3ded532
Aset Telmanov <telmanov2002.at@gmail.com>
2023-08-06 17:05:57 +0500

    attr update

--- attr.js
+++ attr.js
@@ -2,22 +2,36 @@ import { isHTML, validHTML } from "./include.js"
 
 const $ = document.createAttribute.bind(document)
 
+/**
+ * @param {string} name
+ * @param {string} value
+ * @return {Attr}
+ */
 export function attr(name, value) {
-  const _ = $(name)
-  _.value = value
+  const _ = $(name || "_")
+  _.value = value || ""
   return _
 }
 
+/**
+ * @param {HTMLElement} element
+ * @param {string} attrName
+ */
 attr.remove = function (element, attrName) {
   validHTML(element)
   if (attrName instanceof Attr) {
     element.removeAttributeNode(attrName)
   } else {
-    element.removeAttribute(attrName, attrValue)
+    element.removeAttribute(attrName)
   }
   return element
 }
 
+/**
+ * @param {HTMLElement} element
+ * @param {string} attrName
+ * @param {string} attrValue
+ */
 attr.add = attr.set = function (element, attrName, attrValue) {
   validHTML(element)
   if (attrName instanceof Attr) {
@@ -32,14 +46,27 @@ attr.add = attr.set = function (element, attrName, attrValue) {
   return element
 }
 
+/**
+ * @param {HTMLElement} element
+ * @param {string} attrName
+ * @param {boolean} [force]
+ */
 attr.toggle = function (element, attrName, force) {
-  return validHTML(element).toggleAttribute(attrName, force)
+  return validHTML(element).toggleAttribute(attrName, force || undefined)
 }
 
+/**
+ * @param {HTMLElement} element
+ * @param {string} attrName
+ */
 attr.get = function (element, attrName) {
   return validHTML(element).getAttribute(attrName)
 }
 
+/**
+ * @param {HTMLElement} element
+ * @param {string} attrName
+ */
 attr.has = function (element, attrName) {
   return validHTML(element).hasAttribute(attrName)
 }
